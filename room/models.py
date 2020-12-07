5c510d61d72fabd24dc27c84f9c295d8c37d3631
Roman Yakimenko <r.yakimenko.73@gmail.com>
2020-12-07 15:52:42 +0200

    complicated the function for generating the room number

--- room/models.py
+++ room/models.py
@@ -8,7 +8,7 @@ def generate_unique_code():
 	lenght = 6
 
 	while True:
-		code = ''.join(random.choices(string.ascii_uppercase, k=lenght))
+		code = ''.join(random.choices(string.ascii_uppercase+string.ascii_lowercase+'0123456789', k=lenght))
 		if Room.objects.filter(code=code).count() == 0:
 			break
 	return code		
@@ -17,26 +17,26 @@ def generate_unique_code():
 class Room(models.Model):
 	""" Модель описания комнаты """
 	code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+	host = models.CharField(max_length=50, unique=True)
+	
 	created_at = models.DateTimeField(auto_now_add=True)
 
-	host = models.CharField(max_length=50, unique=True)
-
-	moder_can_add = models.BooleanField(null=False, default=False)
-	moder_can_remove = models.BooleanField(null=False, default=False)
-	moder_can_move = models.BooleanField(null=False, default=False)
-	moder_can_playpause = models.BooleanField(null=False, default=False)
-	moder_can_seek = models.BooleanField(null=False, default=False)
-	moder_can_skip = models.BooleanField(null=False, default=False)
-	moder_can_use_chat = models.BooleanField(null=False, default=False)
+	moder_can_add = models.BooleanField(null=False, default=True)
+	moder_can_remove = models.BooleanField(null=False, default=True)
+	moder_can_move = models.BooleanField(null=False, default=True)
+	moder_can_playpause = models.BooleanField(null=False, default=True)
+	moder_can_seek = models.BooleanField(null=False, default=True)
+	moder_can_skip = models.BooleanField(null=False, default=True)
+	moder_can_use_chat = models.BooleanField(null=False, default=True)
 	moder_can_kick = models.BooleanField(null=False, default=False)
 
 	guest_can_add = models.BooleanField(null=False, default=False)
 	guest_can_remove = models.BooleanField(null=False, default=False)
 	guest_can_move = models.BooleanField(null=False, default=False)
 	guest_can_playpause = models.BooleanField(null=False, default=False)
-	guest_can_seek = models.BooleanField(null=False, default=False)
+	guest_can_seek = models.BooleanField(null=False, default=True)
 	guest_can_skip = models.BooleanField(null=False, default=False)
-	guest_can_use_chat = models.BooleanField(null=False, default=False)
+	guest_can_use_chat = models.BooleanField(null=False, default=True)
 	guest_can_kick = models.BooleanField(null=False, default=False)
 
 	def __str__(self):
